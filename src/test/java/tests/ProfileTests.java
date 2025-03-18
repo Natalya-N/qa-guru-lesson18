627f7a6c2ffcc1699c9cb770c5e1f830cc7c5dfd
Nechaeva Natalya <nechaevanatascha@yandex.ru>
2025-03-19 00:46:33 +0300

    Added ui+api tests

--- src/test/java/tests/ProfileTests.java
+++ src/test/java/tests/ProfileTests.java
@@ -2,9 +2,8 @@ package tests;
 
 import api.LoginApi;
 import data.TestData;
-import io.restassured.response.Response;
-import io.restassured.response.ResponseBody;
 import models.AuthModel;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Cookie;
 
@@ -13,37 +12,9 @@ import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
 import static io.qameta.allure.Allure.step;
-import static io.restassured.RestAssured.baseURI;
-import static io.restassured.RestAssured.given;
-import static io.restassured.http.ContentType.JSON;
-import static org.hamcrest.Matchers.is;
 
+@Tag("simple")
 public class ProfileTests extends TestBase {
-
-    @Test
-    void successfulLoginTest() {
-        String requestBody = "{ \"userName\": \"test123456\", \"password\": \"test123456\" }";
-
-        Response authResponse = given()
-                .log().uri()
-                .log().body()
-                .contentType(JSON)
-                .body(requestBody)
-                .when()
-                .post(baseURI + "/Account/v1/Login")
-                .then()
-                .log().status()
-                .log().body()
-                .statusCode(200)
-                .extract().response();
-
-        open("/books");
-
-        getWebDriver().manage().addCookie(new Cookie("userID", authResponse.path("UserId")));
-        getWebDriver().manage().addCookie(new Cookie("expires", authResponse.path("expires")));
-        getWebDriver().manage().addCookie(new Cookie("token", authResponse.path("token")));
-        open("/profile");
-    }
 
     @Test
     void deleteBookFromProfileTest() {
